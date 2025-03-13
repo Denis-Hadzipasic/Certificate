60e15ffda9595b258e803c9c3e7716d9f9f15b61
unknown <denis.hadzipasic@partyrent.com>
2025-03-13 21:56:28 +0100

    a

--- client/src/components/admin/ProductListCard.jsx
+++ client/src/components/admin/ProductListCard.jsx
@@ -1,12 +1,12 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 import axiosClient from "../../utils/axiosClient";
 
 export default function ProductListCard({ product }) {
-    axiosClient.delete(`product/deleteProduct/${product._id}`).then(() => {
-        console.log("deleted").catch((error) => {
-            console.log(error)
-        })
-    })
+  axiosClient.delete(`product/deleteProduct/${product._id}`).then(() => {
+    console.log("deleted").catch((error) => {
+      console.log(error);
+    });
+  });
 
   return (
     <tr className="text-center">
@@ -26,28 +26,21 @@ export default function ProductListCard({ product }) {
         </div>
       </td>
       <td className="px-6 py-4 whitespace-nowrap">
-        <div className="text-sm font-medium text-gray-900">
-          TBD
-        </div>
+        <div className="text-sm font-medium text-gray-900">TBD</div>
       </td>
       <td className="px-6 py-4 whitespace-nowrap">
-        <div className="text-sm font-medium text-gray-900">
-        {product.info}
-
-        </div>
+        <div className="text-sm font-medium text-gray-900">{product.info}</div>
       </td>
       <td className="px-6 py-4 whitespace-nowrap">
         <div className="flex gap-2 justify-center">
-        <NavLink to={`/admin/editProduct/${product._id}`} className="text-sm font-medium text-gray-900">
-        Edit
-
-        </NavLink>
-        <div className="text-sm font-medium text-gray-900">
-        Delete
-
+          <NavLink
+            to={`/admin/editProduct/${product._id}`}
+            className="text-sm font-medium text-gray-900"
+          >
+            Edit
+          </NavLink>
+          <div className="text-sm font-medium text-gray-900">Delete</div>
         </div>
-        </div>
-
       </td>
     </tr>
   );
